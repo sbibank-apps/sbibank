542205d841e5ac257c98b1e138baeeaee099e7fe
GangadharKeralli <gangadharkeralli98@gmail.com>
2022-11-15 23:49:47 +0530

    Updated

--- Employeesservice.java
+++ Employeesservice.java
@@ -7,5 +7,8 @@ let us check what happens in the development branch.
 Using master branch am updating file. 
 let us check what happens in the development branch.
 Git stash command is using
+<<<<<<< HEAD
 good afternoon
 hello world
+=======
+>>>>>>> da15830 (Employeesservice.java)
